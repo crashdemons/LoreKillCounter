324837b27c688f75ffb7e8f662b5a6d5c927f1cf
crashdemons <crashenator@gmail.com>
2020-04-13 18:38:15 -0500

    docs

---
+++ src/main/java/com/github/crashdemons/lorekillcounter/MTTools.java
@@ -0,0 +1,25 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/ .
+ */
+package com.github.crashdemons.lorekillcounter;
+
+import com.github.crashdemons.miningtrophies.events.SimulatedBlockBreakEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.block.BlockBreakEvent;
+
+/**
+ *
+ * @author crashdemons (crashenator at gmail.com)
+ */
+public class MTTools {
+    private MTTools(){}
+    public static boolean isPresent(){
+        return Bukkit.getPluginManager().getPlugin("MiningTrophies")!=null;
+    }
+    public static boolean isSimulatedBlockBreak(BlockBreakEvent event){
+        if(!isPresent()) return false;
+        return (event instanceof SimulatedBlockBreakEvent);
+    }
+}
